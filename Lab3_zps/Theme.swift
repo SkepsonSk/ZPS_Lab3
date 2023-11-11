bf16115f7f4eff8561937600cf8f062e9b7b57c7
Jakub Zelmanowicz <jakub.zelmanowicz@britenet.eu>
2023-11-11 11:00:52 +0100

    Created Themes

---
+++ Lab3_zps/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Lab3_zps
+//
+//  Created by Jakub Zelmanowicz on 11/11/2023.
+//
+
+import Foundation
---
+++ Lab3_zps/ThemeButton.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButton.swift
+//  Lab3_zps
+//
+//  Created by Jakub Zelmanowicz on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ThemeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButton()
+}
